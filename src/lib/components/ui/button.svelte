82d9d0b530dc19f22e9a6b5b010fb0b1a1553610
max-got <max.gottschalk@mailbox.org>
2024-10-23 10:36:46 +0200

    Initial Commit

---
+++ src/lib/components/ui/button.svelte
@@ -0,0 +1,95 @@
+<script lang="ts" module>
+	import { type VariantProps, tv } from 'tailwind-variants';
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
+
+	import type { WithElementRef } from 'bits-ui';
+
+	export const buttonVariants = tv({
+		base: 'inline-flex items-center justify-center whitespace-nowrap rounded-lg text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+		variants: {
+			variant: {
+				default:
+					'bg-primary text-primary-foreground shadow-sm shadow-black/[0.04] hover:bg-primary/90',
+				destructive:
+					'bg-destructive text-destructive-foreground shadow-sm shadow-black/[0.04] hover:bg-destructive/90',
+				outline:
+					'border border-input bg-background shadow-sm shadow-black/[0.04] hover:bg-accent hover:text-accent-foreground',
+				secondary:
+					'bg-secondary text-secondary-foreground shadow-sm shadow-black/[0.04] hover:bg-secondary/80',
+				ghost: 'hover:bg-accent hover:text-accent-foreground',
+				link: 'text-primary underline-offset-4 hover:underline'
+			},
+			size: {
+				default: 'h-9 px-4 py-2',
+				sm: 'h-8 rounded-md px-3 text-xs',
+				lg: 'h-10 rounded-md px-8',
+				icon: 'h-9 w-9'
+			}
+		},
+		defaultVariants: {
+			variant: 'default',
+			size: 'default'
+		}
+	});
+
+	export type ButtonVariant = VariantProps<typeof buttonVariants>['variant'];
+	export type ButtonSize = VariantProps<typeof buttonVariants>['size'];
+
+	export type ButtonProps = WithElementRef<HTMLButtonAttributes> &
+		WithElementRef<HTMLAnchorAttributes> & {
+			variant?: ButtonVariant;
+			size?: ButtonSize;
+			icon?: Snippet;
+		};
+</script>
+
+<script lang="ts">
+	import { cn } from '$lib/utils.js';
+	import type { Snippet } from 'svelte';
+
+	let {
+		class: className,
+		variant = 'default',
+		size = 'default',
+		ref = $bindable(null),
+		href = undefined,
+		type = 'button',
+		children,
+		icon,
+		...restProps
+	}: ButtonProps = $props();
+</script>
+
+{#if href}
+	<a
+		bind:this={ref}
+		class={cn(buttonVariants({ variant, size, className }))}
+		{href}
+		{...restProps}
+		data-button
+	>
+		{@render icon?.()}
+		{@render children?.()}
+	</a>
+{:else}
+	<button
+		bind:this={ref}
+		class={cn(buttonVariants({ variant, size, className }))}
+		{type}
+		data-button
+		{...restProps}
+	>
+		{@render icon?.()}
+		{@render children?.()}
+	</button>
+{/if}
+
+<style lang="postcss">
+	[data-button]:global(:has(svg)) {
+		@apply gap-1;
+
+		& :global(svg) {
+			@apply bottom-0 size-5;
+		}
+	}
+</style>
