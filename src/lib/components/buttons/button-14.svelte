8b06c266f0008a8ebcc961ac7f6e22be1e29d607
max-got <max.gottschalk@mailbox.org>
2024-10-25 13:14:57 +0200

    wtf

--- src/lib/components/buttons/button-14.svelte
+++ src/lib/components/buttons/button-14.svelte
@@ -3,9 +3,8 @@
 
 	import Button from '$lib/components/ui/button.svelte';
 	import LoaderCircle from 'lucide-svelte/icons/loader-circle';
-	import { onMount } from 'svelte';
 
-	let isLoading = false;
+	let isLoading = $state(false);
 
 	const handleClick = () => {
 		isLoading = true;
@@ -14,26 +13,18 @@
 			isLoading = false;
 		}, 1000); // Reset after 1 second
 	};
-
-	// Workaround for hydration mismatch
-	let mounted = false;
-	onMount(() => {
-		mounted = true;
-	});
 </script>
 
-{#if mounted}
-	<Button
-		on:click={handleClick}
-		disabled={isLoading}
-		data-loading={isLoading}
-		class="group relative disabled:opacity-100"
-	>
-		<span class="group-data-[loading=true]:text-transparent">Click me</span>
-		{#if isLoading}
-			<div class="absolute inset-0 flex items-center justify-center">
-				<LoaderCircle class="animate-spin" size={16} strokeWidth={2} aria-hidden="true" />
-			</div>
-		{/if}
-	</Button>
-{/if}
+<Button
+	onclick={handleClick}
+	disabled={isLoading}
+	data-loading={isLoading}
+	class="group relative disabled:opacity-100"
+>
+	<span class="group-data-[loading=true]:text-transparent">Click me</span>
+	{#if isLoading}
+		<div class="absolute inset-0 flex items-center justify-center">
+			<LoaderCircle class="animate-spin" size={16} stroke-width={2} aria-hidden="true" />
+		</div>
+	{/if}
+</Button>
