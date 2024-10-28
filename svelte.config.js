546a9f0192967ad2c4227cfdd75f575542510e3e
Max G. <max.gottschalk@mailbox.org>
2024-10-28 15:40:32 +0100

    feat: add assets

--- svelte.config.js
+++ svelte.config.js
@@ -6,12 +6,21 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	compilerOptions: {
+		warningFilter: (warning) => {
+			if (warning.code === 'a11y_img_redundant_alt') return false;
+		}
+	},
 
 	kit: {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			$lib: './src/lib',
+			$assets: './src/lib/assets'
+		}
 	}
 };
 
