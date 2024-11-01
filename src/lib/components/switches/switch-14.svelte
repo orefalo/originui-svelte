0c6cd153d79022296b92d43a954d14217cc39059
saya <sametcetinkaya00s@gmail.com>
2024-11-01 14:51:29 +0300

    switch-13 & switch-14

--- src/lib/components/switches/switch-14.svelte
+++ src/lib/components/switches/switch-14.svelte
@@ -7,7 +7,7 @@
 	<div class="relative inline-grid h-9 grid-cols-[1fr_1fr] items-center text-sm font-medium">
 		<Switch
 			id="switch-14"
-			checked={false}
+			checked
 			class="peer absolute inset-0 h-[inherit] w-auto rounded-lg data-[state=unchecked]:bg-input/50 [&_span]:z-10 [&_span]:h-full [&_span]:w-1/2 [&_span]:rounded-md [&_span]:transition-transform [&_span]:duration-300 [&_span]:[transition-timing-function:cubic-bezier(0.16,1,0.3,1)] data-[state=checked]:[&_span]:translate-x-full rtl:data-[state=checked]:[&_span]:-translate-x-full"
 		/>
 		<span
