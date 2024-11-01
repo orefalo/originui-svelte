b41c6836e97fe4c9280a1328a1aa61507bf062e9
saya <sametcetinkaya00s@gmail.com>
2024-11-01 14:52:42 +0300

    checkbox-09

--- src/lib/components/checkboxes/checkbox-09.svelte
+++ src/lib/components/checkboxes/checkbox-09.svelte
@@ -3,7 +3,7 @@
 	import Label from '$lib/components/ui/label.svelte';
 </script>
 
-<div class="flex items-center gap-2">
+<div class="flex items-center justify-between gap-2">
 	<Checkbox id="checkbox-09" class="order-1" />
 	<Label for="checkbox-09">Right aligned checkbox</Label>
 </div>
