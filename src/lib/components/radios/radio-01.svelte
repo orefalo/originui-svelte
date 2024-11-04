61cbb1969adb9747c4ea5b6bbc5dadd60a13e24a
Max G. <max.gottschalk@mailbox.org>
2024-11-04 11:02:09 +0100

    fix: radio 1,2,3 labels

--- src/lib/components/radios/radio-01.svelte
+++ src/lib/components/radios/radio-01.svelte
@@ -6,14 +6,14 @@
 <RadioGroup value="r1">
 	<div class="flex items-center gap-2">
 		<RadioGroupItem value="r1" id="radio-01-r1" />
-		<Label for="radio-01-r1">Small</Label>
+		<Label for="radio-01-r1">Option 1</Label>
 	</div>
 	<div class="flex items-center gap-2">
 		<RadioGroupItem value="r2" id="radio-01-r2" />
-		<Label for="radio-01-r2">Medium</Label>
+		<Label for="radio-01-r2">Option 2</Label>
 	</div>
 	<div class="flex items-center gap-2">
 		<RadioGroupItem value="r3" id="radio-01-r3" />
-		<Label for="radio-01-r3">Large</Label>
+		<Label for="radio-01-r3">Option 3</Label>
 	</div>
 </RadioGroup>
--- src/lib/components/radios/radio-03.svelte
+++ src/lib/components/radios/radio-03.svelte
@@ -6,14 +6,14 @@
 <RadioGroup value="r2" disabled>
 	<div class="flex items-center gap-2">
 		<RadioGroupItem value="r1" id="radio-03-r1" />
-		<Label for="radio-03-r1">Small</Label>
+		<Label for="radio-03-r1">Option 1</Label>
 	</div>
 	<div class="flex items-center gap-2">
 		<RadioGroupItem value="r2" id="radio-03-r2" />
-		<Label for="radio-03-r2">Medium</Label>
+		<Label for="radio-03-r2">Option 2</Label>
 	</div>
 	<div class="flex items-center gap-2">
 		<RadioGroupItem value="r3" id="radio-03-r3" />
-		<Label for="radio-03-r3">Large</Label>
+		<Label for="radio-03-r3">Option 3</Label>
 	</div>
 </RadioGroup>
