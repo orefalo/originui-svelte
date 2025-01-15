34dbe89c26c5e7a41960abed7539747e62bad6af
Max G. <64729700+max-got@users.noreply.github.com>
2025-01-15 07:52:07 +0100

    fix: update select-content styling to use dynamic width variable for improved responsiveness (#51)

--- src/lib/components/ui/select/select-content.svelte
+++ src/lib/components/ui/select/select-content.svelte
@@ -22,7 +22,7 @@
 		bind:ref
 		{sideOffset}
 		class={cn(
-			'relative z-50 max-h-[min(24rem,var(--bits-select-content-available-height))] min-w-[8rem] overflow-hidden rounded-lg border border-input bg-popover text-popover-foreground shadow-lg shadow-black/5 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 [&_[role=group]]:py-1',
+			'relative z-50 max-h-[min(24rem,var(--bits-select-content-available-height))] min-w-[var(--bits-select-anchor-width,_8rem)] overflow-hidden rounded-lg border border-input bg-popover text-popover-foreground shadow-lg shadow-black/5 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 [&_[role=group]]:py-1',
 			className
 		)}
 		{...restProps}
